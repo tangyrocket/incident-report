c5f31036216c3d745ca22c01f243d4a6c204eb4f
Alessandro <adim180716@gmail.com>
2024-06-05 19:21:58 -0500

    INICIO

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('reportes', function () {
+    return 'LISTADO DE REPORTES';
+})->middleware(['auth', 'verified'])->name('reportes');
+
+Route::get('reportes/{slug}', function ($slug) {
+    return $slug;
+})->middleware(['auth', 'verified'])->name('incidente');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
