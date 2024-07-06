69e2db2b263d0ce7aa43d398e801d155c452d48d
Alessandro <adim180716@gmail.com>
2024-07-06 18:10:25 -0500

    css for production

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/images',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
