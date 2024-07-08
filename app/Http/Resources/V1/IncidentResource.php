8ab4933ea81675dcad56dd1737b015de1591691a
Alessandro <adim180716@gmail.com>
2024-07-07 19:01:07 -0500

    get incident

--- app/Http/Resources/V1/IncidentResource.php
+++ app/Http/Resources/V1/IncidentResource.php
@@ -15,6 +15,7 @@ class IncidentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'nombre' => $this->title,
             'descripcion' => $this->description,
 
